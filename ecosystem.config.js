685d0d70bce2896c9524f276ce7cb811bfa2a7ab
abdoos <berrabderahim@gmail.com>
2024-01-01 19:21:16 +0100

    adding ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "directus_wanesni",
+      script: "npx",
+      args: "directus start",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
